c07b2a2090d82a3d086dbbc0c3810e578cf2f5ad
jesusdab <117136578+jesusdab@users.noreply.github.com>
2024-12-24 15:00:16 -0500

    Add files via upload

---
+++ gift.js
@@ -0,0 +1,4 @@
+document.querySelector('.click').addEventListener('click', function() {
+    const giftBox = document.querySelector('.click');
+    giftBox.classList.toggle('open'); // Alterna la clase 'open' para abrir/cerrar la tapa
+});
